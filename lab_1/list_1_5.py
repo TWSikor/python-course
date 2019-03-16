e9d95eca2836b4e8cc05148fd4714d63fce07b0b
TWSikor <48251588+TWSikor@users.noreply.github.com>
2019-03-16 16:26:35 +0100

    Delete list_1_5.py

--- lab_1/list_1_5.py
+++
@@ -1,5 +0,0 @@
-#5 upload the solution as a Github repository. I suggest creating a directory for the whole python course and subdirectories lab1, lab2 etc. (0.5p)
-#Ad 5 Hint write in Google "how to create a github repo". There are plenty of tutorials explaining this matter.
-
-# https://github.com/TWSikor/python-course.git
-
