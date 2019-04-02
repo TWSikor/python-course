99d5e7081dc01fff77eef4c07a05032959a6539f
TWSikor <48251588+TWSikor@users.noreply.github.com>
2019-04-02 21:06:12 +0200

    Add files via upload

--- lab_3/list_3_1.py
+++ lab_3/list_3_1.py
@@ -18,36 +18,25 @@ def countField(type, X, Y):
             print('The area of the circle is: ', pi * X ** 2)
         else:
             print('Please enter the correct radius of the circle (radius >= 0)')
-            X = float(input('Please enter the radius of the circle: '))
-            print('The area of the circle is: ', pi * X ** 2)
 
     elif type == 'rectangle':
         if X >= 0 and Y >= 0:
             print('The area of the rectangle is: ', X * Y)
         else:
             print('Please enter the correct sides of the rectangle (side >= 0)')
-            X = float(input('Please enter the first side of the rectangle: '))
-            Y = float(input('Please enter the other side of the rectangle: '))
-            print('The area of the rectangle is: ', X * Y)
 
     elif type == 'triangle':
         if X >= 0 and Y >= 0:
             print('The area of the triangle is: ', 0.5 * X * Y)
         else:
             print('Please enter the correct sides of the triangle (side >= 0)')
-            X = float(input('Please enter the base of the triangle: '))
-            Y = float(input('Please enter the height of the triangle: '))
-            print('The area of the triangle is: ', 0.5 * X * Y)
 
     elif type == 'rhombus':
         if X >= 0 and Y >= 0:
             print('The area of the rhombus is: ', 0.5 * X * Y)
         else:
             print('Please enter the correct diagonals of the rhombus (side >= 0)')
-            X = float(input('Please enter the first diagonal of the rhombus: '))
-            Y = float(input('Please enter the other diagonal of the rhombus: '))
-            print('The area of the rhombus is: ', 0.5 * X * Y)
     else:
         print('Please enter the correct type of geometric figure (e.g. circle, rectangle, triangle, rhombus): ')
 
-countField('rectangle', -0.1 ,-0.1)
+countField('rhombus', -0.1 ,-0.1)
