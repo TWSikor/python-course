72890a1d975c67829079ea357d1eb0686a896226
Wojciech  Sikora <48251588+W-Sikora@users.noreply.github.com>
2019-04-28 04:24:04 +0200

    Add files via upload

--- lab_4/list_4_5.py
+++ lab_4/list_4_5.py
@@ -1,95 +1,81 @@
-# 5
-# ----------------------------------------------------------------------------------------------------------------------
-# Create your own 3d function with multiple local optima.
-# 5_1
-
-import numpy as np
+from numpy import*
 from matplotlib.pyplot import plot, show, legend, figure, cm, contour, clabel
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.optimize import fmin
-from numpy.random import randn, randint
+from numpy.random import rand, randint
 
-def ackley_function(x_1, x_2):
-    """
-    :return:
-    """
-    a = 20
-    b = 0.2
-    c = 2 * np.pi
-    d = 0.5
+def function(w):
+    x = w[0]
+    y = w[1]
+    return sin(x) * x ** 2  + 1 * (y - 0.5 * x)  ** 2 + (0.5 * x * y) - 1.5 * x + cos(y)
 
-    sum_1 = x_1 ** 2 + x_2 ** 2
-    sum_2 = np.cos(c * x_1) + np.cos(c * x_2)
 
-    part_1 = - a * np.exp(- b * (d * sum_1 ** (0.5)))
-    part_2 = - np.exp(d * sum_2)
+T = 300
+X = linspace(- 3, 3, T)
+Y = linspace(- 3, 3, T)
+X, Y = meshgrid(X, Y)
+w = [X, Y]
+Z = function(w)
 
-    return part_1 + part_2 + a + np.exp(1)
 
-# parameters
-T = 1000
-X = np.linspace(- 20, 20, T)
-Y = np.linspace(- 20, 20, T)
-
-X, Y = np.meshgrid(X, Y)
-Z = ackley_function(X, Y)
-
-fig = figure()
-ax = Axes3D(figure(figsize = (8, 5)))
-ax.plot_surface(X, Y, Z, cmap = cm.coolwarm, linewidth = 0.4)
-ax.text2D(0.05, 0.95, '3d Ackley function with multiple local optima', transform = ax.transAxes)
+ax = Axes3D(figure(figsize = (10, 7)))
+ax.plot_surface(X, Y, Z, cmap = cm.rainbow, linewidth = 0.4)
+ax.text2D(0.05, 0.95, '3d function function with multiple local optima', transform = ax.transAxes)
 show()
 
-# ----------------------------------------------------------------------------------------------------------------------
-# Create an algorithm which takes an initial point and looks for the closest local optimum (1p)
-# 5_2
-"""
-def gradient_descent_for_ackley_function():
 
-    initial_point = randint(- 15, 15) * randn(2)
-    rate = 0.01
-    precision = 0.000001
-    previous_step_size = 1
-    max_iters = 10000
-    iteration_counter = 0
-    dx_1 = lambda x_1, :
-    dx_2 = lambda x_2,:
-    while previous_step_size > precision and iteration_counter < max_iters:
-        x_1 = initial_point[0]
-        x_2 = initial_point[1]
-        new_x_1 = new_x_1 - rate * dx_1(x_1)
-        new_x_2 = new_x_2 - rate * dx_2(x_2)
-        previous_step_size_1 = abs(new_x_1 - x_1)
-        previous_step_size_2 = abs(new_x_2 - x_2)
-        iteration_counter = iteration_counter + 1
-        print('iteration: ', iteration_counter, '\n Z value is: ', )
-    print('The local minimum is', new_x_1, new_x_2 )
-"""
-initial_point = randint(- 15, 15) * randn(2)
-T = 1000
-X = np.linspace(- 20, 20, T)
-Y = np.linspace(- 20, 20, T)
-
-X, Y = np.meshgrid(X, Y)
-Z = ackley_function(X, Y)
-
-figure()
+figure(figsize = (10, 7))
 CS = contour(X, Y, Z)
-clabel(CS, inline = 1, fontsize = 15)
-plot(initial_point[0],initial_point[1],'ko', label = 'initial point')
-#plot(x_min[0],x_min[1],'ko', label='final')
+clabel(CS, inline = 1, fontsize = 10)
+show()
+
+
+initial_random_point = randint(-3, 3) * rand(2)
+print('initial_random_point =', initial_random_point, '\n')
+x_min = fmin(function, initial_random_point)
+
+figure(figsize = (10, 7))
+CS = contour(X, Y, Z)
+clabel(CS, inline = 1, fontsize = 10)
+plot(initial_random_point[0], initial_random_point[1],'ko', label ='initial point')
+plot(x_min[0], x_min[1], 'ro', label = 'local minimum point')
 legend()
 show()
 
-# ----------------------------------------------------------------------------------------------------------------------
-# Create an algorithm which aims to find a global optimum, the time of execution is limiter to ~30sec (1p)
-# 5_3
 
-# ----------------------------------------------------------------------------------------------------------------------
-# If your solution is heuristic test its quality. Measure the probability of finding the GLOBAL optimum (1p).
-# 5_4
+list_of_minima_arg = []
+list_of_minima_val = []
 
-# ----------------------------------------------------------------------------------------------------------------------
-# You can, for example, execute your search function multiple times and check if the returned result is what you expected.
-# Measure the success / total trials rate (2p)
-# 5_5
+N = 10000
+
+for i in range(N):
+    random_value = rand()
+
+    if random_value <= 0.25:
+        initial_random_point = [- 3 * rand(), - 3 * rand()]
+        list_of_minima_arg.append(initial_random_point)
+
+    elif random_value <= 0.50:
+        initial_random_point = [ 3 * rand(), - 3 * rand()]
+        list_of_minima_arg.append(initial_random_point)
+
+    elif random_value <= 0.75:
+        initial_random_point = [- 3 * rand(), 3 * rand()]
+        list_of_minima_arg.append(initial_random_point)
+
+    elif random_value <= 1.00:
+        initial_random_point = [ 3 * rand(), 3 * rand()]
+        list_of_minima_arg.append(initial_random_point)
+
+for i in range(N):
+    list_of_minima_val.append(function(list_of_minima_arg[i]))
+
+j = list_of_minima_val.index(min(list_of_minima_val))
+min_glob = list_of_minima_arg[j]
+
+figure(figsize = (10, 7))
+CS = contour(X, Y, Z)
+clabel(CS, inline = 1, fontsize = 10)
+plot(min_glob[0], min_glob[1],'ro', label =' global minimum point')
+legend()
+show()
